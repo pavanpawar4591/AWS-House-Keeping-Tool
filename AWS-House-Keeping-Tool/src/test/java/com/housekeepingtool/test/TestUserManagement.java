cf686796e9aa1321438b34684236f52150730bc2
pavan_pawar <pavanpawar4591@gmail.com>
2017-02-11 18:45:48 +0530

    Junit All user List Test case

--- AWS-House-Keeping-Tool/src/test/java/com/housekeepingtool/test/TestUserManagement.java
+++ AWS-House-Keeping-Tool/src/test/java/com/housekeepingtool/test/TestUserManagement.java
@@ -3,6 +3,10 @@ package com.housekeepingtool.test;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -36,5 +40,33 @@ public class TestUserManagement {
 		Boolean result = userService.addUser(Mockito.any(User.class));
 		assertEquals(result, true);
 
+	}
+
+	@Test
+	public void testListAllUser() throws BusinessException {
+		when(useDao.getAllUser()).thenReturn(getDummyListUser());
+
+		List<User> ulist = userService.listAllUsers();
+		assertEquals(ulist.size(), 1);
+	}
+
+	private User getDummyUser() {
+		User u = new User();
+		u.setCreatedBy("pavan");
+		u.setUid(1);
+		u.setCreateOn(new Date());
+		u.setLastName("pawar");
+		u.setPassword("abcd");
+		u.setRole(1);
+		u.setIsActive(1);
+		u.setUpdatedBy("admin");
+		return u;
+
+	}
+
+	private List<User> getDummyListUser() {
+		List<User> ulist = new ArrayList<>();
+		ulist.add(getDummyUser());
+		return ulist;
 	}
 }

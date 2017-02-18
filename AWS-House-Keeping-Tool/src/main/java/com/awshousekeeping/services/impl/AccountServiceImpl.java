d67417e76c2e836d2f0a35fffa023100c453aebe
Anuja Chivate <anuja.chivate67@gmail.com>
2017-02-18 21:48:47 +0530

    Account

--- AWS-House-Keeping-Tool/src/main/java/com/awshousekeeping/services/impl/AccountServiceImpl.java
+++ AWS-House-Keeping-Tool/src/main/java/com/awshousekeeping/services/impl/AccountServiceImpl.java
@@ -1,5 +1,30 @@
 package com.awshousekeeping.services.impl;
 
-public class AccountServiceImpl {
+import java.util.List;
 
+import com.awshousekeeping.dao.AccountDao;
+import com.awshousekeeping.dao.impl.AccountDaoImpl;
+import com.awshousekeeping.model.Account;
+import com.awshousekeeping.services.AccountService;
+import com.awshousekeeping.utils.BusinessException;
+
+public class AccountServiceImpl implements AccountService {
+
+	private AccountDao AccountDao = new AccountDaoImpl(); 
+
+	public List<Account> getAllAccount() throws BusinessException {
+		
+		return AccountDao.getAllAccount();
+	}
+	
+	@Override
+	public boolean addNewAWSAccount(Account a) throws BusinessException {
+		
+	
+		// TODO access key secret key ecrypt using aes 256
+		// TODO 20 input string 16 xx last four visible
+		// TODO call dao accountService 
+		return AccountDao.AddAccount(a);		
+	}
+	 
 }

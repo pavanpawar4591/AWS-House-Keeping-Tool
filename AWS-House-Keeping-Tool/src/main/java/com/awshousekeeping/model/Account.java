33f724218fb878fd42cbb97cb966bdc2a939d865
Anuja Chivate <anuja.chivate67@gmail.com>
2017-02-18 21:52:13 +0530

    Account

--- AWS-House-Keeping-Tool/src/main/java/com/awshousekeeping/model/Account.java
+++ AWS-House-Keeping-Tool/src/main/java/com/awshousekeeping/model/Account.java
@@ -1,186 +1,163 @@
 package com.awshousekeeping.model;
 
+import java.util.Date;
+
 public class Account {
 
 	private int accountId;
-	private int createdOn;
+	private Date createdOn;
 	private String createdBy;
-	private int updatedOn;
+	private Date updatedOn;
 	private String updatedBy;
 	private int hspsId;
-	private String projectExpireDate;
-	private String freeTrialExpireDate;
+	private Date projectExpireDate;
+	private Date freeTrialExpireDate;
 	private String awsAccountOwnerName;
-	private String hspsExpireDate;
+	private Date hspsExpireDate;
 	private String emailIdOfOwner;
 	private String projectName;
 	private int projectId;
 	private String accountType;
 	private String hspsDescription;
 	private int businessUnit;
-	private int AWSAccountNumber;
-	private String AWSAccountAlias;
-	private int AWSAccessKey;
-	private int AWSSecretKey;
-
+	private int awsAccountNumber;
+	private String awsAccountAlias;
+	private int awsAccessKey;
+	private int awsSecretKey;
+	private String awsAccessKeyXXXX;
+	private String awsSecretKeyXXXX;
+	
 	public int getAccountId() {
 		return accountId;
 	}
-
 	public void setAccountId(int accountId) {
 		this.accountId = accountId;
 	}
-
-	public int getCreatedOn() {
+	public Date getCreatedOn() {
 		return createdOn;
 	}
-
-	public void setCreatedOn(int createdOn) {
+	public void setCreatedOn(Date createdOn) {
 		this.createdOn = createdOn;
 	}
-
 	public String getCreatedBy() {
 		return createdBy;
 	}
-
 	public void setCreatedBy(String createdBy) {
 		this.createdBy = createdBy;
 	}
-
-	public int getUpdatedOn() {
+	public Date getUpdatedOn() {
 		return updatedOn;
 	}
-
-	public void setUpdatedOn(int updatedOn) {
+	public void setUpdatedOn(Date updatedOn) {
 		this.updatedOn = updatedOn;
 	}
-
 	public String getUpdatedBy() {
 		return updatedBy;
 	}
-
 	public void setUpdatedBy(String updatedBy) {
 		this.updatedBy = updatedBy;
 	}
-
 	public int getHspsId() {
 		return hspsId;
 	}
-
 	public void setHspsId(int hspsId) {
 		this.hspsId = hspsId;
 	}
-
-	public String getProjectExpireDate() {
+	public Date getProjectExpireDate() {
 		return projectExpireDate;
 	}
-
-	public void setProjectExpireDate(String projectExpireDate) {
+	public void setProjectExpireDate(Date projectExpireDate) {
 		this.projectExpireDate = projectExpireDate;
 	}
-
-	public String getFreeTrialExpireDate() {
+	public Date getFreeTrialExpireDate() {
 		return freeTrialExpireDate;
 	}
-
-	public void setFreeTrialExpireDate(String freeTrialExpireDate) {
+	public void setFreeTrialExpireDate(Date freeTrialExpireDate) {
 		this.freeTrialExpireDate = freeTrialExpireDate;
 	}
-
 	public String getAwsAccountOwnerName() {
 		return awsAccountOwnerName;
 	}
-
 	public void setAwsAccountOwnerName(String awsAccountOwnerName) {
 		this.awsAccountOwnerName = awsAccountOwnerName;
 	}
-
-	public String getHspsExpireDate() {
+	public Date getHspsExpireDate() {
 		return hspsExpireDate;
 	}
-
-	public void setHspsExpireDate(String hspsExpireDate) {
+	public void setHspsExpireDate(Date hspsExpireDate) {
 		this.hspsExpireDate = hspsExpireDate;
 	}
-
 	public String getEmailIdOfOwner() {
 		return emailIdOfOwner;
 	}
-
 	public void setEmailIdOfOwner(String emailIdOfOwner) {
 		this.emailIdOfOwner = emailIdOfOwner;
 	}
-
 	public String getProjectName() {
 		return projectName;
 	}
-
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
-
 	public int getProjectId() {
 		return projectId;
 	}
-
 	public void setProjectId(int projectId) {
 		this.projectId = projectId;
 	}
-
 	public String getAccountType() {
 		return accountType;
 	}
-
 	public void setAccountType(String accountType) {
 		this.accountType = accountType;
 	}
-
 	public String getHspsDescription() {
 		return hspsDescription;
 	}
-
 	public void setHspsDescription(String hspsDescription) {
 		this.hspsDescription = hspsDescription;
 	}
-
 	public int getBusinessUnit() {
 		return businessUnit;
 	}
-
 	public void setBusinessUnit(int businessUnit) {
 		this.businessUnit = businessUnit;
 	}
-
-	public int getAWSAccountNumber() {
-		return AWSAccountNumber;
+	public int getAwsAccountNumber() {
+		return awsAccountNumber;
 	}
-
-	public void setAWSAccountNumber(int aWSAccountNumber) {
-		AWSAccountNumber = aWSAccountNumber;
+	public void setAwsAccountNumber(int awsAccountNumber) {
+		this.awsAccountNumber = awsAccountNumber;
 	}
-
-	public String getAWSAccountAlias() {
-		return AWSAccountAlias;
+	public String getAwsAccountAlias() {
+		return awsAccountAlias;
 	}
-
-	public void setAWSAccountAlias(String aWSAccountAlias) {
-		AWSAccountAlias = aWSAccountAlias;
+	public void setAwsAccountAlias(String awsAccountAlias) {
+		this.awsAccountAlias = awsAccountAlias;
 	}
-
-	public int getAWSAccessKey() {
-		return AWSAccessKey;
+	public int getAwsAccessKey() {
+		return awsAccessKey;
 	}
-
-	public void setAWSAccessKey(int aWSAccessKey) {
-		AWSAccessKey = aWSAccessKey;
+	public void setAwsAccessKey(int awsAccessKey) {
+		this.awsAccessKey = awsAccessKey;
 	}
-
-	public int getAWSSecretKey() {
-		return AWSSecretKey;
+	public int getAwsSecretKey() {
+		return awsSecretKey;
 	}
-
-	public void setAWSSecretKey(int aWSSecretKey) {
-		AWSSecretKey = aWSSecretKey;
+	public void setAwsSecretKey(int awsSecretKey) {
+		this.awsSecretKey = awsSecretKey;
 	}
-
+	public String getAwsAccessKeyXXXX() {
+		return awsAccessKeyXXXX;
+	}
+	public void setAwsAccessKeyXXXX(String awsAccessKeyXXXX) {
+		this.awsAccessKeyXXXX = awsAccessKeyXXXX;
+	}
+	public String getAwsSecretKeyXXXX() {
+		return awsSecretKeyXXXX;
+	}
+	public void setAwsSecretKeyXXXX(String awsSecretKeyXXXX) {
+		this.awsSecretKeyXXXX = awsSecretKeyXXXX;
+	}	
+	
 }

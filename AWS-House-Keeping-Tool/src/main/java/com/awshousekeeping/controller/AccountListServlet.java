c972bc229673920a336d6154051a35f6a2b1d0b7
Pavan_Pawar <pavan_pawar@persistent.co.in>
2017-04-22 16:01:50 +0530

    sonar buf fix

--- AWS-House-Keeping-Tool/src/main/java/com/awshousekeeping/controller/AccountListServlet.java
+++ AWS-House-Keeping-Tool/src/main/java/com/awshousekeeping/controller/AccountListServlet.java
@@ -8,16 +8,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.awshousekeeping.services.AccountService;
 import com.awshousekeeping.services.impl.AccountServiceImpl;
 
+@SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/Account-List.do")
-
+/**
+ * 
+ * @author pavan_pawar
+ * 
+ * This Servlet for account management 
+ *
+ */
 public class AccountListServlet extends HttpServlet {
 
-	private AccountServiceImpl accountServiceImpl = new AccountServiceImpl();
-
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException{
+			throws ServletException, IOException {
+		AccountService accountServiceImpl = new AccountServiceImpl();
 		request.setAttribute("accounts", accountServiceImpl.listAllAccounts());
 		request.getRequestDispatcher("/WEB-INF/views/Account-List.jsp").forward(request, response);
 	}
